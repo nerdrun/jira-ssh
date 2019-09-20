eba6163f0dd846279e027a872c8690d4730646ee
Madcore-Kai <nerdrun.kai@gmail.com>
2019-09-20 15:46:19 +0900

    Create confluence-server.sh

---
+++ confluence-server.sh
@@ -0,0 +1,3 @@
+wget https://www.atlassian.com/software/confluence/downloads/binary/atlassian-confluence-7.0.1-x64.bin
+chmod a+x atlassian-confluence-7.0.1-x64.bin
+sudo ./atlassian-confluence-7.0.1-x64.bin
