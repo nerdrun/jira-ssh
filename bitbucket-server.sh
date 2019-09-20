a15b21686896ebd41912b2557eb870f499729246
Madcore-Kai <nerdrun.kai@gmail.com>
2019-09-20 15:46:43 +0900

    Create bitbucket-server.sh

---
+++ bitbucket-server.sh
@@ -0,0 +1,3 @@
+wget https://www.atlassian.com/software/stash/downloads/binary/atlassian-bitbucket-6.6.1-x64.bin
+chmod a+x atlassian-bitbucket-6.6.1-x64.bin
+sudo ./atlassian-bitbucket-6.6.1-x64.bin
