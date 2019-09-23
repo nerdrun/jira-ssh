7867983f07e8bef41b4ebb7fb6454aa6118ab298
Madcore-Kai <nerdrun.kai@gmail.com>
2019-09-23 10:40:41 +0900

    Update jira-server.sh

--- jira-server.sh
+++ jira-server.sh
@@ -2,8 +2,8 @@
 
 wget https://www.atlassian.com/software/jira/downloads/binary/atlassian-jira-software-8.3.3-x64.bin
 
-chmod a+x atlassian-jira-software-8.2.2-x64.bin
+chmod a+x atlassian-jira-software-8.3.3-x64.bin
 
-sudo ./atlassian-jira-software-8.2.2-x64.bin
+sudo ./atlassian-jira-software-8.3.3-x64.bin
 
 JAVA_HOME="/opt/atlassian/jira/jre/bin"
