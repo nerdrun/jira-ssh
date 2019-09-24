75ae24a39a37edccff61d00f43b1c11eab8c5f43
Madcore-Kai <nerdrun.kai@gmail.com>
2019-09-24 16:07:27 +0900

    Update jira-server.sh

--- jira-server.sh
+++ jira-server.sh
@@ -6,7 +6,7 @@ chmod a+x atlassian-jira-software-8.3.3-x64.bin
 
 sudo ./atlassian-jira-software-8.3.3-x64.bin
 
-PATH=$PATH:/opt/atlassian/jira/jre/bin
+echo JAVA_HOME="/opt/atlassian/jira/jre/bin"
 
 createdb -E UNICODE -l C -T template0 jiradb
 createdb -E UNICODE -l C -T template0 confluencedb
