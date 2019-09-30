70205059ba91f76687249ab11be96b37a489c29f
Madcore-Kai <nerdrun.kai@gmail.com>
2019-09-30 13:56:28 +0900

    Update jira-server.sh

--- jira-server.sh
+++ jira-server.sh
@@ -3,7 +3,7 @@ echo "Started to install JIRA server"
 
 wget https://www.atlassian.com/software/jira/downloads/binary/atlassian-jira-software-8.3.3-x64.bin
 
-chmod a+x atlassian-jira-software-8.3.3-x64.bin
+sudo chmod a+x atlassian-jira-software-8.3.3-x64.bin
 
 sudo ./atlassian-jira-software-8.3.3-x64.bin
 
@@ -15,7 +15,7 @@ echo "Started to install CONFLUENCE server"
 
 wget https://www.atlassian.com/software/confluence/downloads/binary/atlassian-confluence-7.0.1-x64.bin
 
-chmod a+x atlassian-confluence-7.0.1-x64.bin
+sudo chmod a+x atlassian-confluence-7.0.1-x64.bin
 
 sudo ./atlassian-confluence-7.0.1-x64.bin
 
@@ -25,21 +25,21 @@ echo "Started to install BITBUCKET server"
 
 wget https://www.atlassian.com/software/stash/downloads/binary/atlassian-bitbucket-6.6.1-x64.bin
 
-chmod a+x atlassian-bitbucket-6.6.1-x64.bin
+sudo chmod a+x atlassian-bitbucket-6.6.1-x64.bin
 
 sudo ./atlassian-bitbucket-6.6.1-x64.bin
 
 # POSTGRES
 
-echo "Started to install postgres"
+#echo "Started to install postgres"
 
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+#sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 
-wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add - 
+#wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add - 
 
-sudo apt-get update -y
+#sudo apt-get update -y
 
-sudo apt-get install postgresql-9.6 -y
+#sudo apt-get install postgresql-9.6 -y
 
 #sudo su - postgres
 
